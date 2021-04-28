1a56c964e3f6aee5c65eeb9f4e517955fd2ef264
Souluon <epson5503@gmail.com>
2021-04-28 21:44:36 +0600

    First Model

--- src/shop/models.py
+++ src/shop/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 
-class Product:
+class Product(models.Model):
     name = models.CharField(max_length = 200, verbose_name = 'Название')
     price = models.DecimalField(max_digits = 7, decimal_places=0, verbose_name = 'Цена')
     count = models.IntegerField()
     avaliable = models.BooleanField(default = True)
-    description = models.TextField(max_length = 255, verbose_name = 'Описание')
+    description = models.TextField(max_length = 1000, verbose_name = 'Описание')
