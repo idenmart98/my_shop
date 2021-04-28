a1cdce707b8fbe283fc221269d9b2218bc7e8725
aseru <adel.derbisheva@gmail.com>
2021-04-28 22:50:08 +0600

    add new model Category

--- src/shop/models.py
+++ src/shop/models.py
@@ -2,9 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+    name = models.TextField(max_length = 100, verbose_name = 'Заголовок')
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    def __str__(self):
+        return f"{self.name}"
+
 class Product(models.Model):
     name = models.CharField(max_length = 200, verbose_name = 'Название')
     price = models.DecimalField(max_digits = 7, decimal_places=0, verbose_name = 'Цена')
     count = models.IntegerField()
     avaliable = models.BooleanField(default = True)
     description = models.TextField(max_length = 1000, verbose_name = 'Описание')
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
+    
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+
+    def __str__(self):
+        return f"{self.name}"
