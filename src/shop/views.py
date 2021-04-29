d7def08247e7661ceccc596277f34b2f7d26d7a6
Dasifue <dasifue@gmail.com>
2021-04-29 12:23:57 +0600

    all_products

--- src/shop/views.py
+++ src/shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from .models import Product
+from rest_framework.generics import ListAPIView
+from .serializers.products import ProductsListSerializer
 # Create your views here.
+
+class ProductstListView(ListAPIView):
+    serializer_class = ProductsListSerializer
+    queryset = Product.objects.all()
