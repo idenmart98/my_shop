b8cb3229d5c90bcfba861e810ed0250fe472bf05
JANAR123 <nazarbekovjanar@gmail.com>
2021-04-30 18:54:34 +0600

    src

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product,Image
 
 # Register your models here.
 
 admin.site.register(Product)
+
+class ImageInline(admin.TabularInline):
+    fk_name = 'product'
+    model = Image
+
+class TrouserAdmin(admin.ModelAdmin):
+    inlines = [ImageInline,]
--- src/shop/models.py
+++ src/shop/models.py
@@ -8,3 +8,11 @@ class Product(models.Model):
     count = models.IntegerField()
     avaliable = models.BooleanField(default = True)
     description = models.TextField(max_length = 1000, verbose_name = 'Описание')
+
+class Image(models.Model):
+    product= models.ForeignKey(Product, related_name = 'photo', on_delete = models.CASCADE)
+    photo = models.ImageField(upload_to='images', blank=True)
+
+    class Meta:
+        verbose_name = 'Фото'
+        verbose_name_plural = 'Фото'
