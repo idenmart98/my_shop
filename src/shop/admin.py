b6526a455d7e7eef86b4f27b1590854a87f59537
JANAR123 <nazarbekovjanar@gmail.com>
2021-04-30 20:38:58 +0600

    src

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -3,12 +3,13 @@ from .models import Product,ProductImage
 
 
 admin.site.register(ProductImage)
-admin.site.register(Product)
+
+
 
 class ProductImageAdmin(admin.StackedInline):
     model = ProductImage
  
-
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     inlines = [ProductImageAdmin]
  
