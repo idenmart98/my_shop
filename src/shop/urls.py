bc0cab935001ca3344e30cc078d8dff10072c36e
aseru <adel.derbisheva@gmail.com>
2021-04-30 21:28:44 +0600

    new def for category_detail

--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import ProductDetailView, ProductstListView
+from .views import ProductDetailView, ProductstListView, category_details
 
 app_name = 'shop'
 
 urlpatterns = [
     path('product/', ProductstListView.as_view()),
     path('product/<int:product_id>', ProductDetailView.as_view()),
+    path('category/<int:category_id>', category_details)
 ]
--- src/shop/views.py
+++ src/shop/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Product
 from .serializers.product import ProductDetailSerializer, ProductsListSerializer
 from rest_framework.generics import ListAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 # Create your views here.
 
 class ProductDetailView(RetrieveUpdateDestroyAPIView):
@@ -14,3 +16,11 @@ class ProductstListView(ListAPIView):
     serializer_class = ProductsListSerializer
     queryset = Product.objects.all()
 
+@api_view(['GET'])
+def category_details(request, category_id):
+    products = Product.objects.filter(category__id = category_id)
+    print(products)
+    if products:
+        ser = ProductsListSerializer(products, many = True)
+        return Response({'data': ser.data})
+    return Response({'data': 'error'})
