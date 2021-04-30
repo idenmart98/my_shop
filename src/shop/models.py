749f8d443277d0321f418cd45b9eb60e4b84984a
Dasifue <dasifue@gmail.com>
2021-04-30 21:46:03 +0600

    i tyred

--- src/shop/models.py
+++ src/shop/models.py
@@ -41,21 +41,21 @@ class ProductImage(models.Model):
 class Card(models.Model):
     costummer = models.CharField(max_length=30, verbose_name='владелец')
     number = models.CharField(max_length=20, verbose_name='номер владельца')
+    def __str__(self):
+        return f"{self.costummer}"
     class Meta:
         verbose_name = 'Корзина'
         verbose_name_plural = 'Корзины'
-    def __str__(self):
-        return f"{self.costummer}"
 
 class CardProduct(models.Model):
     product = models.ForeignKey(Product, related_name='card_product', on_delete=models.CASCADE)
     card = models.ForeignKey(Card, related_name='card_product', on_delete=models.CASCADE)
     count = models.IntegerField()
+    def __str__(self):
+        return f"{self.product}"
     class Meta:
         verbose_name = 'Продукт в корзине'
         verbose_name_plural = 'Продукты в корзине'
-    def __str__(self):
-        return f"{self.product}"
     
 
 
