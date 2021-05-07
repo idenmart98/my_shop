6230fef5430cdd62baf0062e53b91f26011a9c2b
Dasifue <dasifue@gmail.com>
2021-05-07 21:50:38 +0600

    init serializers

---
+++ src/shop/serializers/__init__.py
@@ -0,0 +1,4 @@
+from .cart import ProductSerializer, CartSerializer
+from .category import CategoryDetailSerializer, CategoryListSerializers
+from .product import ProductImagesSerializer, ProductDetailSerializer, ProductsListSerializer
+from .review import ReviewCreateSerializer
